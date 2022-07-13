d0667daac4e68158ac3c71160823d575e52020e3
Lucas Colwell <colwellc37@gmail.com>
2022-07-13 17:25:05 -0600

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
     get '/', to: 'welcome#index'
 
     get '/airports', to:'airports#index'
     get '/airports/:id', to:'airports#show'
-
+    get '/airports/:airport_id/planes', to: 'planes#index'
     get '/planes', to:'planes#index'
     get '/planes/:id', to:'planes#show'
-
     get '/passengers', to:'passengers#index'
     get '/passengers/:id', to:'passengers#show'
-
-
 end
